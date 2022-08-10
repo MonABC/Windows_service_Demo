21e26d42c1dcaa08dc0a406e836b7ea30741bd2c
Viet. Mai Tien - CMC Global DJ2 <mtviet@cmcglobal.vn>
2022-08-10 10:54:00 +0700

    thay doi General Startup type -> Automatic va Log On -> chon Local System account

---
+++ WindowsService01090822/ProjectInstaller.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace WindowsService01090822
+{
+    partial class ProjectInstaller
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.serviceProcessInstaller1 = new System.ServiceProcess.ServiceProcessInstaller();
+            this.serviceProcessInstaller1.Account = System.ServiceProcess.ServiceAccount.LocalSystem; //.LocalSystem thì trong mục Log On sẽ tick vào Local System account
+            this.serviceInstaller1 = new System.ServiceProcess.ServiceInstaller();
+            // 
+            // serviceProcessInstaller1
+            // 
+            this.serviceProcessInstaller1.Password = null;
+            this.serviceProcessInstaller1.Username = null;
+            // 
+            // serviceInstaller1
+            // 
+            this.serviceInstaller1.ServiceName = "ServiceName01090822"; // tên service
+            this.serviceInstaller1.Description = "ServiceDescription01090822"; // mô tả
+            this.serviceInstaller1.DisplayName = "ServiceDisplayName01090822"; // hiện thị tên
+            this.serviceInstaller1.StartType = System.ServiceProcess.ServiceStartMode.Automatic; // thay đổi General 
+
+            // 
+            // ProjectInstaller
+            // 
+            this.Installers.AddRange(new System.Configuration.Install.Installer[] {
+            this.serviceProcessInstaller1,
+            this.serviceInstaller1});
+
+
+        }
+
+        #endregion
+
+        private System.ServiceProcess.ServiceProcessInstaller serviceProcessInstaller1;
+        private System.ServiceProcess.ServiceInstaller serviceInstaller1;
+    }
+}
---
+++ WindowsService01090822/Service1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace WindowsService01090822
+{
+    public partial class Service1 : ServiceBase
+    {
+        Timer timer = new Timer();
+        public Service1()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            WriteLog("thoi gian bat dau chay: " + DateTime.Now);
+            timer.Elapsed += new ElapsedEventHandler(TimeInterval);
+            timer.Interval = 10000;
+            timer.Enabled = true;
+            
+        }
+        private void TimeInterval(object source, ElapsedEventArgs e)
+        {
+            WriteLog("thoi gian lap lai cua service: "+ DateTime.Now);
+        }
+
+        protected override void OnStop()
+        {
+            WriteLog("thoi gian dung service: " + DateTime.Now);
+        }
+
+        private void WriteLog(string message)
+        {
+            string path = AppDomain.CurrentDomain.BaseDirectory + "\\Logs\\";
+            if (!System.IO.Directory.Exists(path)) 
+            {
+                System.IO.Directory.CreateDirectory(path);
+            }
+            string file = path + DateTime.Now.ToString("dd-MM-yyyy") +".txt";
+            if (!System.IO.File.Exists(file))
+            {
+                using (StreamWriter sw = File.CreateText(file))
+                {
+                    sw.WriteLine(message);  
+                }
+
+            }
+            else
+            {
+                using (StreamWriter sw = File.AppendText(file))
+                {
+                    sw.WriteLine(message);
+                }
+            }
+        }
+    }
+}
